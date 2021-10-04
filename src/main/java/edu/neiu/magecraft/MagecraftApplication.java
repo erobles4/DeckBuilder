f1dbafa1f6372616db94416bfc9b9aeb738a2035
erobles4 <70727105+erobles4@users.noreply.github.com>
2021-10-03 20:27:25 -0500

    Initial commit

---
+++ src/main/java/edu/neiu/magecraft/MagecraftApplication.java
@@ -0,0 +1,13 @@
+package edu.neiu.magecraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagecraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagecraftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/neiu/magecraft/MagecraftApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neiu.magecraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagecraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
